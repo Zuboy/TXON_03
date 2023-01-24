6125fc26af1db8a4c34d237735633b1d8a163142
Zuboy <85677899+Zuboy@users.noreply.github.com>
2023-01-24 10:45:50 +0530

    Initial commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is capital of india?",
+            "Which one is not the programming language?",
+            "Which task is this for TXON?",
+            "Which company owns the AndriodStudio?"
+    };
+
+    public static String choices[][] = {
+            {"Delhi","Mumbai","Kolkata","Chennai"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Task1","Task2","Task3","Task4"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Delhi",
+            "Notepad",
+            "Task3",
+            "Google"
+    };
+
+}
